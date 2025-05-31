5f9ac819bdc71fdb3922493ac870d7080943c339
SKAkramali <shaikakramali29@gmail.com>
2025-05-31 12:50:54 +0530

    solved issues with login route

--- backend/routes/login.js
+++ backend/routes/login.js
@@ -2,9 +2,20 @@ const express = require("express");
 const router = express.Router();
 const db = require("../database");
 
-router.post("/", (req, res)=> {
-  const data = req.body;
-  console.log(data);
+router.post("/", async(req, res)=> {
+  try{
+    const data = req.body;
+    const query = "insert into logininfo (username, password, email) values(?, ?, ?)";
+    const result = await db.execute(query, [req.body.name, req.body.password, req.body.email]);
+    res.status(200).json({
+      message: "sucessfully logined"
+    });
+    console.log(data);
+  } catch (err){
+    res.status(500).json({
+      error: err
+    });
+  }
 });
 
 module.exports = router;
