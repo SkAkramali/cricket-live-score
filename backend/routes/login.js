5c63ecc10d9e297c8e132791ad3e247b2a65e2fd
SKAkramali <shaikakramali29@gmail.com>
2025-05-31 13:27:59 +0530

    add encryption to password

--- backend/routes/login.js
+++ backend/routes/login.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const db = require("../database");
+const bcrypt = require("bcrypt")
 
 router.post("/", async(req, res)=> {
   try{
     const data = req.body;
+    const hashedPassword = await bcrypt.hash(data.password, 6);
+    console.log(hashedPassword);
     const query = "insert into logininfo (username, password, email) values(?, ?, ?)";
-    const result = await db.execute(query, [data.userName, data.password, data.email]);
+    const result = await db.execute(query, [data.userName, hashedPassword, data.email]);
     res.status(201).json({
       message: "Successfully registered",
     });
