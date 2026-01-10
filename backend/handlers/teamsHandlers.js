b1c594fea7c0f1790be600c4057ae7fff2a57755
SKAkramali <shaikakramali29@gmail.com>
2026-01-10 19:31:44 +0530

    refactor: moved all handlers of teams.js to handler to obtain mv

--- backend/handlers/teamsHandlers.js
+++ backend/handlers/teamsHandlers.js
@@ -5,4 +5,33 @@ const registerTeam = async(data) => {
   return result;
 }
 
-module.exports = registerTeam;
+const getTeamsData = async(req, res)=>{
+  try{
+    const [data] = await db.execute("select * from teams");
+    res.json(data);
+  } catch(err){
+    res.status(500).json({errorMessage: err});
+  }
+}
+
+const registerTeamHandler = async(req, res) =>{
+  try{
+    const result = await registerTeam(req.body);
+    console.log(result);
+    res.status(201).json({message: "Team Has Registerd"});
+  } catch(err){
+    res.status(400).json({error: err});
+  }
+}
+
+const getTeamByIdHandler = async(req, res)=>{
+  const id = req.params.id;
+   try{
+    const [data] = await db.execute("select * from teams where team_id = ?", [id]);
+    return res.status(200).json(data);
+  } catch(err){
+    return res.status(500).json({errorMessage: err});
+  }
+}
+
+module.exports = {registerTeam, getTeamsData, registerTeamHandler, getTeamByIdHandler};
--- backend/routes/teams.js
+++ backend/routes/teams.js
@@ -1,36 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const db = require("../database");
-const registerTeam = require("../handlers/teamsHandlers");
+const {registerTeam, getTeamsData, registerTeamHandler, getTeamByIdHandler} = require("../handlers/teamsHandlers");
 
-router.get("/", async(req, res)=>{
-  try{
-    const [data] = await db.execute("select * from teams");
-    res.json(data);
-  } catch(err){
-    res.status(500).json({errorMessage: err});
-  }
+router.get("/", getTeamsData);
 
-});
+router.post("/", registerTeamHandler);
 
-router.post("/", async(req, res) =>{
-  try{
-    const result = await registerTeam(req.body);
-    console.log(result);
-    res.status(201).json({message: "Team Has Registerd"});
-  } catch(err){
-    res.status(400).json({error: err});
-  }
-});
-
-router.get("/:id", async(req, res)=>{
-  const id = req.params.id;
-   try{
-    const [data] = await db.execute("select * from teams where team_id = ?", [id]);
-    return res.status(200).json(data);
-  } catch(err){
-    return res.status(500).json({errorMessage: err});
-  }
-})
+router.get("/:id", getTeamByIdHandler);
 
 module.exports = router;
